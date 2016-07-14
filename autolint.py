f7bfb7b622fa8f6497eb28d8129039a3d4e2449e
fmontoto <fmontotomonroy@gmail.com>
2016-07-13 22:32:42 -0400

    Fix pep8 errors.

--- autolint.py
+++ autolint.py
@@ -54,8 +54,8 @@ class AutoLint(object):
         if configuration is not None:
             configuration_path = pathlib.Path(configuration).expanduser()
             if not configuration_path.is_file():
-                raise AutoLintIOError(("Expecting a file as configuration, but "
-                                       "got %s" % configuration))
+                raise AutoLintIOError(("Expecting a file as configuration, but"
+                                       " got %s" % configuration))
             self.configuration = configuration_path
 
         if ignore_file is not None:
@@ -69,61 +69,122 @@ class AutoLint(object):
 
     def __load_configuration(self):
         """Load the configuration file into a python object."""
+
         with open(str(self.configuration), 'r') as f:
             self.configuration_dict = yaml.safe_load(f)
 
-    def run_linter(self, pretty_print=True, print_all=False):
-        """Load the configurations and run the linter."""
+    def __pretty_print(self, results):
+        """Print a hierarchy of the languages and linters ran.
+
+        :param results, dict with the results of an execution of __lint.
+
+        :return amount of files with return code != 0
+
+        """
+
+        def print_helper(filename, stdout, stderr):
+            """ Print nicely the parameters
+
+            :param filename: str name of the file.
+            :param stdout: bytes of stdout wrote by the linter.
+            :param stderr: bytes of stderr wrote by the linter.
+            """
+            if result[1] and result[2]:
+                print("\t\t%s\n\t\t\t%s\n\t\t\t%s" % (
+                        filename,
+                        result[1].decode('utf-8').replace('\n', '\n\t\t\t'),
+                        result[2].decode('utf-8').replace('\n', '\n\t\t\t')))
+            elif result[1]:
+                print("\t\t%s\n\t\t\t%s" % (
+                        filename,
+                        result[1].decode('utf-8').replace('\n', '\n\t\t\t')))
+            elif result[2]:
+                print("\t\t%s\n\t\t\t%s" % (
+                        filename,
+                        result[2].decode('utf-8').replace('\n', '\n\t\t\t')))
+
+        total_files = 0
+        failed_files = 0
+        for lang, d1 in results.items():
+            print("%s" % lang)
+            lang_total_files = 0
+            lang_failed_files = 0
+            for linter, d2 in d1.items():
+                linter_total_files = 0
+                linter_failed_files = 0
+                print("\t%s" % linter)
+                for filename, result in d2.items():
+                    linter_total_files += 1
+                    if result[0] != 0:
+                        linter_failed_files += 1
+                        print_helper(filename, result[1], result[2])
+                    else:
+                        print("\t\t%s" % filename)
+                if len(d1) > 1:
+                    print(("\t%s: Checked %d files; %d with errors") % (
+                            linter, linter_total_files, linter_failed_files))
+                lang_total_files += linter_total_files
+                lang_failed_files += linter_failed_files
+            if len(results) > 1:
+                print(("%s: Checked %d files; %d with errors") % (
+                        lang, lang_total_files, lang_failed_files))
+            total_files += lang_total_files
+            failed_files += lang_failed_files
+        print("Checked %d files, %d with errors" % (total_files,
+                                                    failed_files))
+        return failed_files
+
+    def run_linter(self, pretty_print=False, print_all=True):
+        """Load the configurations and run the linter.
+
+        :param pretty_print If true prints a hierarchy of languages,
+                            linters and files describing the execution. It
+                            also summarize the failed files and total files.
+
+        :param print_all If true prints the stdout and stderr for each
+                         execution of the linter. It keeps the order each
+                         output was printed by the linter.
+
+        :return A tuple of two elemts, the first is 1 if there is at leat one
+                linter execution returned a return_code != 0 and 0 otherwise.
+                The second element es the complete result dict as returned by
+                __lint.
+        """
 
         all_files = self.__get_all_files()
         to_lint_files = self.__remove_ignored_files(all_files)
         results = self.__lint(self.__classify_files(to_lint_files))
         return_code = 0
 
-
         if print_all:
             for lang, v in results.items():
-                print("%s" % lang)
                 for linter, r in v.items():
-                    print("\t%s" % linter)
                     for filename, result in r.items():
-                        print('\t\t"%s"' % filename)
-                        print("\t\t\tret:%s\n\t\t\tout:%s\n\t\t\terr:%s" % result)
+                        stdout = result[1].decode('utf-8')
+                        stderr = result[2].decode('utf-8')
+                        sys.stdout.write(stdout)
+                        sys.stderr.write(stderr)
                         if result[0] != 0:
                             return_code = 1
 
         elif pretty_print:
-            total_files = 0
-            failed_files = 0
-            for lang, v in results.items():
-                print("%s" % lang)
-                lang_total_files = 0
-                lang_failed_files = 0
-                for linter, r in v.items():
-                    linter_total_files = 0
-                    linter_failed_files = 0
-                    print("\t%s" % linter)
-                    for filename, result in r.items():
-                        linter_total_files += 1
+            return_code = 1 if self.__pretty_print(results) > 0 else 0
+
+        else:
+            for v in results.itervalues():
+                for r in v.itervalues():
+                    for result in r.itervalue():
                         if result[0] != 0:
-                            linter_failed_files += 1
-                            print("\t\t%s\n\t\t\t%s\n\t\t\t%s" % (filename,
-                                                                  result[1].decode('utf-8').replace('\n', '\n\t\t\t'),
-                                                                  result[2].decode('utf-8').replace('\n', '\n\t\t\t')))
-                    if len(v) > 1:
-                        print("\t%s: Checked %d files, %d with errors" % (linter, linter_total_files, linter_failed_files))
-                    lang_total_files += linter_total_files
-                    lang_failed_files += linter_failed_files
-                if len(results) > 1:
-                    print("%s: Checked %d files, %d with errors" % (lang, lang_total_files, lang_failed_files))
-                total_files +=lang_total_files
-                failed_files += lang_failed_files
-            print("Checked %d files, %d with errors" % (total_files, failed_files))
+                            return 1, results
 
         return return_code, results
 
     def __get_all_files(self):
-        """Returns a list of all the files in the target directory."""
+        """Get all the files in the target directory.
+
+        :return  A list of str with the path of each file under the target
+                 directory.
+        """
 
         ret_files = []
         for root, dirs, files in os.walk(str(self.target)):
@@ -134,11 +195,12 @@ class AutoLint(object):
     def __remove_ignored_files(self, all_files):
         """Remove the files matching a pattern at the ignore file
 
-        :param all_files, list of the files to be removed
+        :param all_files, list of the files to be filtered
 
-        :return a subset of all_files, where all the files matched by an ignore
-                pattern were removed
+        :return a set instance containing a subset of all_files, where all the
+                files matched by an ignore pattern were removed.
         """
+
         if self.ignore_file is None:
             return all_files
 
@@ -158,7 +220,11 @@ class AutoLint(object):
     def __classify_files(self, files):
         """Classify a list of files into languages.
 
-        :param files, list of files to be classified.
+        The classification is against the patterns included in the
+        configuration file per language. Unix filename pattern matching will
+        be applied to the path of the file.
+
+        :param files, an iterable of files to be classified.
 
         :return a dict with an entry per each language found in the directory,
                 excluding ignore files specified. Each entry includes a list of
@@ -188,7 +254,13 @@ class AutoLint(object):
         :param files, dictionary with languages as key and a iterable of files
                       as values.
 
-        :return TODO
+        :return A group of dicts and ordered dicts with the results of the
+                executions ran. The hierarchy is as follows:
+                {<language_name>:
+                    {<linter_name>:
+                        {<file_name>:(return_code, stdout_bytes, stderr_bytes)}
+                    }
+                }
         """
 
         ret = {}
