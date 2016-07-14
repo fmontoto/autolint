09fdcec4c3df7ab46b2e44b5500c7600d4ad6c6c
fmontoto <fmontotomonroy@gmail.com>
2016-07-14 00:20:48 -0400

    Add __init__.py and setup.py

---
+++ autolint/__init__.py
@@ -0,0 +1,15 @@
+
+__author__ = "Francisco Montoto"
+__copyright__ = "Copyright © 2016 Francisco Montoto"
+__created__ = "2016-07-13"
+__email__ = "fmontotomonroy@gmail.com"
+
+__license__ = "MIT"
+__project__ = "autolint"
+__description__ = "Automated run of linter at your repository."
+__url__ = "https://www.github.com/fmontoto/autolint"
+__status__ = "Development"
+__updated__ = "2016-07-13"
+__version__ = "0.0.1"
+__py_modules__ = ['autolint']
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+import autolint
+
+with open('requirements.txt', 'r') as f:
+    __dependencies__ = f.read().strip().split()
+
+__long_description__ = "TODO"
+
+__setup__ = {"name": autolint.__project__,
+             "version": autolint.__version__,
+             "author": autolint.__author__,
+             "author_email": autolint.__email__,
+             "url": autolint.__url__,
+             "description": autolint.__description__,
+             "long_description": __long_description__,
+             "py_modules": autolint.__py_modules__,
+             "install_requires": __dependencies__
+             }
+setuptools.setup(**__setup__)
